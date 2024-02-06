fcfb4dff2cd6e3bdfd5a56c4fd1332c58217513a
Tilak <tilaksk2003@gmail.com>
2024-02-06 13:41:10 +0530

    -

--- 5.py
+++ 5.py
@@ -13,14 +13,10 @@ eroded_image = cv2.erode(image, kernel, iterations=1)
 # Display the original and eroded images
 cv2.imshow('Original Image', image)
 cv2.imshow('Eroded Image', eroded_image)
-cv2.waitKey(0)
 kernel = np.ones((5,5), np.uint8)
 
 # Perform dilation
 dilated_image = cv2.dilate(image, kernel, iterations=1)
-
-# Display the original and dilated images
-cv2.imshow('Original Image', image)
 cv2.imshow('Dilated Image', dilated_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
